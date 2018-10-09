3a0be589623a6dc4c5b4437425fea79db8e068d0
SergioRt1 <sergio200035@gmail.com>
2018-10-08 19:08:38 -0500

    first commit

---
+++ src/main/java/com/eci/arsw/project/Unite/UniteApplication.java
@@ -0,0 +1,12 @@
+package com.eci.arsw.project.Unite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniteApplication.class, args);
+	}
+}
