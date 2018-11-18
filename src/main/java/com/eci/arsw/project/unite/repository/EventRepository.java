01b8d72cf938cb54021cf226947c4c3d4451225d
SergioRt1 <sergio200035@gmail.com>
2018-11-18 01:57:57 -0500

    Correcion cambio de nombre evento

--- src/main/java/com/eci/arsw/project/unite/repository/EventRepository.java
+++ src/main/java/com/eci/arsw/project/unite/repository/EventRepository.java
@@ -2,6 +2,13 @@ package com.eci.arsw.project.unite.repository;
 
 import com.eci.arsw.project.unite.model.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface EventRepository extends MongoRepository<Event, Integer> {
+
+//    @Query(Criteria.where("_id").is())
+//    void chancheNameEvent(String name);
+
 }
